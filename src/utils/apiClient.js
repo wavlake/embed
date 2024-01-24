25f06890ed80f274943c00bf998f7e9324813acc
Michael Rhee <michaelrhee@pm.me>
2024-01-24 12:23:09 -0600

    Change env vars

--- src/utils/apiClient.js
+++ src/utils/apiClient.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const apiProtocol = process.env.NEXT_PUBLIC_WAVLAKE_PROTOCOL;
-const apiHost = process.env.NEXT_PUBLIC_WAVLAKE_HOST;
-const apiPort = process.env.NEXT_PUBLIC_WAVLAKE_PORT;
+const apiProtocol = process.env.NEXT_PUBLIC_CATALOG_PROTOCOL;
+const apiHost = process.env.NEXT_PUBLIC_CATALOG_HOST;
+const apiPort = process.env.NEXT_PUBLIC_CATALOG_PORT;
 const wavlakeApi = `${apiProtocol}://${apiHost}:${apiPort}/v1`;
 
 export default axios.create({
