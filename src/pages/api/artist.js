2fc1b6330929bd95fcbcae04843e1ff41dad3b41
Michael Rhee <michaelrhee@pm.me>
2024-01-24 11:57:39 -0600

    Switch artist api call

--- src/pages/api/artist.js
+++ src/pages/api/artist.js
@@ -1,8 +1,21 @@
 import apiClient from "../../utils/apiClient";
 
 export default async function handler(req, res) {
+  const artistId = await apiClient
+    .get(`/artists/${req.query.artistUrl}/url`)
+    .then(({ data }) => {
+      if (!data?.data?.id) {
+        return res.status(404).json({ err: "Artist not found" });
+      } else if (data?.data?.id) {
+        return data.data.id;
+      }
+    })
+    .catch(({ err }) => {
+      res.status(400).json({ err });
+    });
+
   await apiClient
-    .get(`/catalog/embed/tracks/${req.query.artistUrl}/artist`)
+    .get(`/artists/${artistId}`)
     .then(({ data }) => {
       res.status(200).json(data);
     })
--- src/utils/apiClient.js
+++ src/utils/apiClient.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const apiProtocol = process.env.NEXT_PUBLIC_WAVLAKE_PROTOCOL;
 const apiHost = process.env.NEXT_PUBLIC_WAVLAKE_HOST;
 const apiPort = process.env.NEXT_PUBLIC_WAVLAKE_PORT;
-const wavlakeApi = `${apiProtocol}://${apiHost}:${apiPort}`;
+const wavlakeApi = `${apiProtocol}://${apiHost}:${apiPort}/v1`;
 
 export default axios.create({
   baseURL: wavlakeApi,
