80edb5158f9fb23b87ea6f079a995b5997f621df
Michael Rhee <michaelrhee@pm.me>
2024-01-24 12:03:02 -0600

    Switch track and album calls

--- src/pages/api/album.js
+++ src/pages/api/album.js
@@ -2,9 +2,9 @@ import apiClient from "../../utils/apiClient";
 
 export default async function handler(req, res) {
   await apiClient
-    .get(`/catalog/embed/tracks/${req.query.albumId}/album`)
+    .get(`/tracks/${req.query.albumId}/album`)
     .then(({ data }) => {
-      res.status(200).json(data);
+      res.status(200).json(data.data);
     })
     .catch(({ err }) => {
       res.status(400).json({ err });
--- src/pages/api/track.js
+++ src/pages/api/track.js
@@ -2,7 +2,7 @@ import apiClient from "../../utils/apiClient";
 
 export default async function handler(req, res) {
   await apiClient
-    .get(`/catalog/embed/track/${req.query.trackId}`)
+    .get(`/tracks/${req.query.trackId}`)
     .then(({ data }) => {
       res.status(200).json(data);
     })
