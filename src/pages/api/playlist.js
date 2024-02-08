72f9b2f40520308796ce5b9cb3c5723b42a7e186
Josh Remaley <joshremaley@gmail.com>
2024-02-08 16:13:41 -0300

    update playlist to use catalog

--- src/pages/api/playlist.js
+++ src/pages/api/playlist.js
@@ -1,10 +1,10 @@
-import apiClient from "../../utils/apiClient";
+import catalogClient from "../../utils/catalogClient";
 
 export default async function handler(req, res) {
-  await apiClient
-    .get(`/catalog/embed/playlist/${req.query.playlist}`)
+  await catalogClient
+    .get(`/playlists/${req.query.playlist}`)
     .then(({ data }) => {
-      res.status(200).json(data);
+      res.status(200).json(data.data);
     })
     .catch(({ err }) => {
       res.status(400).json({ err });
