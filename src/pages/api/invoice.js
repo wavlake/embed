8896a5b394c558ad91e14dac8acd6d4ed526be8a
Josh Remaley <joshremaley@gmail.com>
2024-02-14 09:15:11 -0600

    rm logs

--- src/pages/api/invoice.js
+++ src/pages/api/invoice.js
@@ -11,11 +11,9 @@ export default async function handler(req, res) {
   await serverClient
     .post(`/ln/generate-amp-invoice`, req.body)
     .then(({ data }) => {
-      console.log({ data });
       res.status(200).json(data);
     })
     .catch(({ err }) => {
-      console.log({ err });
       res.status(400).json({ err });
     });
 }
