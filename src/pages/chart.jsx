0ddb34980a28148caebff43edceb3d378622a64e
Michael Rhee <77257032+blastshielddown@users.noreply.github.com>
2024-02-16 16:09:20 -0700

    Update src/pages/chart.jsx
    Co-authored-by: joshr4 <joshremaley@gmail.com>

--- src/pages/chart.jsx
+++ src/pages/chart.jsx
@@ -4,8 +4,8 @@ const domain = process.env.NEXT_PUBLIC_EMBED_DOMAIN_URL;
 
 export async function getServerSideProps(context) {
   const { query } = context;
-  const queryString = Object.keys(query)
-    .map((key) => `${key}=${query[key]}`)
+  const queryString = Object.entries(query)
+    .map((entry) => entry.join("="))
     .join("&");
 
   const result = await fetch(`${domain}/api/chart?${queryString}`);
