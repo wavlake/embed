121df5afc5d81429a7ec1a1e7c7510fa1b0151d8
Michael Rhee <michaelrhee@pm.me>
2024-02-21 15:03:54 -0600

    clean up logging and api route

--- src/pages/playlist/[playlistId].jsx
+++ src/pages/playlist/[playlistId].jsx
@@ -4,30 +4,24 @@ import catalogClient from "../../utils/catalogClient";
 const domain = process.env.NEXT_PUBLIC_EMBED_DOMAIN_URL;
 
 export async function getServerSideProps(context) {
-  console.log(context);
   const { playlistId } = context.params;
   const queryString = context.resolvedUrl.split("?")[1];
   const showSats = queryString ? queryString.includes("sort=sats") : false;
 
-  const result = await catalogClient
+  const data = await catalogClient
     // Query params located in second index of split array
     .get(`/playlists/${playlistId}?${queryString}`)
     .then(({ data }) => {
       return data.data;
     })
     .catch(({ err }) => {
-      return err;
+      return {
+        redirect: {
+          destination: "/",
+          permanent: false,
+        },
+      };
     });
-
-  const data = await result;
-  if (!data) {
-    return {
-      redirect: {
-        destination: "/",
-        permanent: false,
-      },
-    };
-  }
 
   return { props: { trackData: data, showSats: showSats } };
 }
